c5c1a9cbf30f223079ffa34541fb72f955b2514b
Lee, Hakjun <ehjun32@gamil.com>
2025-09-16 13:54:53 +0900

    five

--- src/World.java
+++ src/World.java
@@ -1,5 +1,8 @@
 public class World {
     public static void run() {
-      System.out.println("202114090");
+        System.out.println("202114090");
+    }
+    public static void run2() {
+        System.out.println("hello world");
     }
 }
